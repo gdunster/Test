f26885d7fd69fcaf86618fb60b153ede52b37dbe
Gideon Dunster <spudlybutt200@d-108-179-167-180.dhcp4.washington.edu>
2014-11-06 11:53:41 -0800

    First Random Data Set!

---
+++ simple_script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+simple_script.py - generate some random data and then plot a histogram of it
+
+Created by GPD on Nov 6, 2014
+"""
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+print 'Hello there!'
+data = np.random.normal(0,1,2000)
+plt.hist(data,100)
+plt.show()
