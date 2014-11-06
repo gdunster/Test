06fcb0aa1bf6304c4bfa15d1b76335e9e5040bbb
Gideon Dunster <spudlybutt200@d-108-179-167-180.dhcp4.washington.edu>
2014-11-06 12:25:07 -0800

    deleted text, added stuff to make it readable?

--- simple_script.py
+++ simple_script.py
@@ -6,11 +6,28 @@ simple_script.py - generate some random data and then plot a histogram of it
 Created by GPD on Nov 6, 2014
 """
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-print 'Hello there!'
-data = np.random.normal(0,1,200000)
-plt.hist(data,100)
-plt.show()
+
+
+def show_bryant(data_length=200000, bins=100):
+    """Create some data with the normal distribution, then plot it as a histogram
+    Takes:
+        data_length: how many point to generate (200000)
+        bins: how many bins to sort the data into for the histogram (100)
+    Gives:
+        None
+    """
+
+    data = np.random.normal(0,1,data_length)
+    plt.hist(data, bins)
+    plt.show()
+
+
+def main():
+    show_bryant(200000, 100)
+
+
+if __name__ == '__main__':
+    main()
